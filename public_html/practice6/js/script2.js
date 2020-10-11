abb66357b33ca6de2dae56c76551a0263b177ecd
nekitto <30697565+nekitto@users.noreply.github.com>
2020-10-11 17:15:25 +0300

    Create script2.js

---
+++ public_html/practice6/js/script2.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded",
+        function (event) {
+
+        // Unobtrusive event binding
+        document.querySelector(".button2")
+                .addEventListener("click", function () {
+
+
+
+
+
+                // Call server to get the name
+                $ajaxUtils
+                        .sendGetRequest("https://reqres.in/api/users/3",
+                                function (request) {
+                                console.log(request.responseText);
+                                        var personObject = JSON.parse(request.responseText);
+                                        document.querySelector("#content")
+                                        .innerHTML = "<h2>Hello " + personObject.data['first_name']
+                                        + ' ' + personObject.data['last_name'] + "!</h2>";
+                                });
+                });
+        }
+);
