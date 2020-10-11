b227db4bc302d51b8db5c9ab2bd65b737da93e54
nekitto <30697565+nekitto@users.noreply.github.com>
2020-10-11 19:25:58 +0300

    Update script2.js

--- public_html/practice6/js/script2.js
+++ public_html/practice6/js/script2.js
@@ -8,18 +8,17 @@ document.addEventListener("DOMContentLoaded",
 
 
 
-
                 // Call server to get the name
                 $ajaxUtils
                         .sendGetRequest("https://reqres.in/api/users/1",
+                                        $('#button1').click(function(){
+	                                var value = $('#select').val();
                                 function (request) {
                                 console.log(request.responseText);
                                         var personObject = JSON.parse(request.responseText);
-                                        var name = $('#select').val();
                                         document.querySelector("#content")
                                         .innerHTML = "<h2>Hello " + personObject.data['first_name']
-                                        + ' ' + personObject.data['last_name'] + "!</h2>
-                                        <h3>name</h3>";
+                                        + ' ' + personObject.data['last_name'] +  value"!</h2>";
                                 });
                 });
         }
