39f7a6de2357d040dec0fae4d196f49a85293590
nekitto <30697565+nekitto@users.noreply.github.com>
2020-10-11 17:09:32 +0300

    Create scriptBTN1.js

---
+++ public_html/practice6/js/scriptBTN1.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button1")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.txt", 
+            function (request) {
+              var name = request.responseText;
+
+              document.querySelector("#content")
+                .innerHTML = "<h2><b>test<></h2>";
+            });
+
+        
+      });
+  }
+);
