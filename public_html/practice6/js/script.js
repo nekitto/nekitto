6e5368a3a7609d7eb64b56f4a4cb112b698425bb
nekitto <30697565+nekitto@users.noreply.github.com>
2020-10-12 02:43:28 +0300

    Update script.js

--- public_html/practice6/js/script.js
+++ public_html/practice6/js/script.js
@@ -19,10 +19,11 @@ var i = 1;
                                         .innerHTML ='<h1>Nikita Sitar</h1><p>nikita_sitar@ukr.net</p><img src="img.png" alt=""/>';
                                       
                                      }else if (i > 12){
+                                         i = 1;
                                          document.querySelector("#header1")
                                         .innerHTML = "<h1>" + personObject.data['first_name']
                                         + ' ' + personObject.data['last_name'] + "</h1>"+"<p>"+personObject.data['email']+"</p>"+"<img src="+personObject.data['avatar']+">";
-                                         i = 0;
+                                         
                                      }                       
                                      i=i+1;
                                 });
