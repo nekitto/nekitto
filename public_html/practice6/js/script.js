dee14c23bc9e79944e2a7f6a17487db7d131e2bf
nekitto <30697565+nekitto@users.noreply.github.com>
2020-10-12 03:07:38 +0300

    Update script.js

--- public_html/practice6/js/script.js
+++ public_html/practice6/js/script.js
@@ -1,11 +1,12 @@
 var i = 1;
+    var clicks = 0;
     document.addEventListener("DOMContentLoaded",
         function (event) {
 
         
         document.querySelector(".header1")
                 .addEventListener("click", function () {
-
+                clicks = clicks + 1;
                 $ajaxUtils
                         .sendGetRequest("https://reqres.in/api/users/"+i,
                                 function (request) {
@@ -13,17 +14,22 @@ var i = 1;
                                         var personObject = JSON.parse(request.responseText);
                                         document.querySelector("#header1")
                                         .innerHTML = "<h1>" + personObject.data['first_name']
-                                        + ' ' + personObject.data['last_name'] + "</h1>"+"<p>"+personObject.data['email']+"</p>"+"<img src="+personObject.data['avatar']+">";                                                                                
-                                     if(i%5==0){
+                                        + ' ' + personObject.data['last_name'] + "</h1>"+"<p>"+personObject.data['email']+"</p>"+"<img src="+personObject.data['avatar']+">";
+                                        
+                                        i=i+1;
+                                        
+                                     if(clicks%5==0){
                                          document.querySelector("#header1")
                                         .innerHTML ='<h1>Nikita Sitar</h1><p>nikita_sitar@ukr.net</p><img src="img.png" alt=""/>';
                                       
                                      }else if (i > 12){
-                                         i = 0;
                                          
-                                         
+                                         i = 1;
+                                         document.querySelector("#header1")
+                                        .innerHTML = "<h1>" + personObject.data['first_name']
+                                        + ' ' + personObject.data['last_name'] + "</h1>"+"<p>"+personObject.data['email']+"</p>"+"<img src="+personObject.data['avatar']+">";
                                      }                       
-                                     i=i+1;
+                                     
                                 });
                 });
         }
